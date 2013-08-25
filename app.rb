d3ca31d8b7a73e15aba060c24fdf801f183b4dff
Inderpal Singh <Inderpal@INinderps.local>
2013-08-25 23:40:38 +0530

    added app.rb

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'bundler'
+require 'json'
+
+Bundler.require
+
+if ENV['VCAP_SERVICES']
+  require 'json'
+  services = JSON.parse(ENV['VCAP_SERVICES'])
+  postgresql_key = services.keys.select { |svc| svc =~ /postgresql/i }.first
+  postgresql = services[postgresql_key].first['credentials']
+  postgresql_conn = "postgres://"+postgresql['user']+":"+postgresql['password']+ \
+    "@"+postgresql['host']+":"+postgresql['port']+"/"+postgresql['name']
+  DataMapper.setup(:default, postgresql_conn)
+else
+  DataMapper.setup(:default, "postgres://photonetra:photonetra@localhost:5432/photonetra")
+end
+
+class Client
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text
+  property :phone, Text
+  property :email, Text
+  property :address, Text
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+Client.auto_upgrade!
+
+get '/' do
+  content_type :json
+  @clients = Client.all(:order => [:id.desc])
+  @clients.to_json
+end
